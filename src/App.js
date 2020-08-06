c90f0995e21e460432fb2f987e4dc618ac91811a
James Schubach <schubach.james@gmail.com>
2020-08-06 14:52:25 +1000

    Added basic bootstrap header and footer for website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Create Import File
 import './App.css';
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+import { Navbar, Form, InputGroup, FormControl, Button } from "react-bootstrap";
 
-class Footer extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                <footer className="footer-area">
-                </footer>
-            </React.Fragment>
-        )
-    }
+class Footer extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar fixed="bottom" className="bg-light justify-content-between">
+          <Form inline>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Form>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
 export default Footer;

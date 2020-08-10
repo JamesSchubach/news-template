a2066f88c5f67818de3834d12c45c73a45d495ee
James Schubach <schubach.james@gmail.com>
2020-08-10 10:12:42 +1000

    Updated footer for main theme

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,24 +7,8 @@ class Footer extends Component {
       <React.Fragment>
         <Navbar fixed="bottom" className="bg-light justify-content-between">
           <Form inline>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                placeholder="Username"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
-          </Form>
-          <Form inline>
-            <FormControl
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-            <Button type="submit">Submit</Button>
+            <FormControl type="text" placeholder="Email" className=" mr-sm-2" />
+            <Button type="submit">Subscribe</Button>
           </Form>
         </Navbar>
       </React.Fragment>

17389a332f5f8a6741fb7872a764ae0900d8f75e
aseem-thakar <41178612+aseem-thakar@users.noreply.github.com>
2020-08-10 11:09:26 +1000

    feat: skeleton Featured Articles on home

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import Featured from "./components/HomeLayout/Featured";
 
+{
+  /* Main Page*/
+}
 
+class Main extends Component {
+  render() {
+    return (
+      <div id="main-root" className="">
+        <Header
+          headertransparent="header--transparent"
+          colorblack="color--black"
+          logoname="logo.png"
+        />
 
-{/* Main Page*/}
+        {/* Start Banner Area */}
+        <div className="banner-area"></div>
+        {/* Start Featured Articles Area */}
+        <div className="featured-area">
+          <Featured />
+        </div>
 
-class Main extends Component{
-    render(){
-        return(
-            <div id="main-root" className=""> 
-                <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
-                
-                {/* Start Banner Area */}
-                <div className="banner-area">
+        {/* Start Filtered Articles Area */}
+        <div className="filtered-area"></div>
 
-                </div>
-                {/* Start Featured Articles Area */}
-                <div className="featured-area">
-                    
-                </div>
-
-                {/* Start Filtered Articles Area */}
-                <div className="filtered-area">
-                    
-                </div>
-
-
-                {/* Start Browse Area */}
-                <div className="browse-area">
-                    
-                </div>
-                <Footer />
-                
-            </div>
-        )
-    }
+        {/* Start Browse Area */}
+        <div className="browse-area"></div>
+        <Footer />
+      </div>
+    );
+  }
 }
 export default Main;
